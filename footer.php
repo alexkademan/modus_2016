6348332208ec09dfea87eb735c504446436c53ce
Alex Kademan <alexkademan@gmail.com>
2016-04-20 17:02:16 -0500

    installed basic javascript

--- footer.php
+++ footer.php
@@ -11,16 +11,17 @@
 
 ?>
 
-	</div><!-- #content -->
+<span id="mobile_shader" class="mobile_shader"></span>
+</div><!-- end #page -->
+<footer class="site-footer">
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+  <div class="outer">
+
+    footer content
+
+  </div>
+
+</footer>
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -24,30 +24,29 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
+	<header id="site_header" class="site_header">
+	  <div class="outside">
+	    <span class=stripe>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+	      <span class="toggle_menu">
+	        <div class="line line-1"></div>
+	  			<div class="line line-2"></div>
+	  			<div class="line line-3"></div>
+	        <a class="toggle"></a>
+	      </span>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+	      <a class="site-title" href="{{ site.baseurl }}">logo</a>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-	
-	<div id="content" class="site-content">
+	    </span>
+
+	    <nav id="site-nav" class="site-nav">
+	      <ul id="mainNav" class="mainNav">
+	        <li><a class="page-link" href="#">Link</a></li>
+
+	      </ul>
+	    </nav>
+
+	  </div>
+
+	</header>
+	<div id="page" class="page">
