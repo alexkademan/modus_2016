0429b203e2fc292e5974581d82d7f2297b01873c
Alex Kademan <alexkademan@gmail.com>
2016-04-20 15:45:51 -0500

    adjusted font issue, configured font-awesome

--- header.php
+++ header.php
@@ -49,5 +49,5 @@
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
-
+	
 	<div id="content" class="site-content">
